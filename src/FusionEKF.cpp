74b01375d76d02817023b17a8a1e931736872e8e
Justin Lee <justin.lee007@gmail.com>
2017-04-18 15:29:14 -0700

    Set H_radar and use pow

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -117,16 +117,15 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;    //dt - expressed in seconds
   previous_timestamp_ = measurement_pack.timestamp_;
 
-  float dt_2 = dt * dt;
-  float dt_3 = dt_2 * dt;
-  float dt_4 = dt_3 * dt;
+  float dt_2 = pow(dt, 2);
+  float dt_3 = pow(dt, 3);
+  float dt_4 = pow(dt, 4);
 
   // Modify the F matrix so that the time is integrated
   ekf_.F_(0, 2) = dt;
   ekf_.F_(1, 3) = dt;
 
   // set the process covariance matrix Q
-  ekf_.Q_ = MatrixXd(4, 4);
   ekf_.Q_ << dt_4 / 4 * noise_ax_, 0, dt_3 / 2 * noise_ax_, 0,
       0, dt_4 / 4 * noise_ay_, 0, dt_3 / 2 * noise_ay_,
       dt_3 / 2 * noise_ax_, 0, dt_2 * noise_ax_, 0,
@@ -140,7 +139,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
-    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+    H_radar_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = H_radar_;
     ekf_.R_ = R_radar_;
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
